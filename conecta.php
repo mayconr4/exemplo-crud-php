95986744c342c90a5ddad78c21f6997bf46496ea
mayconr4 <mayconhs2007@gmail.com>
2025-03-14 10:52:43 -0300

    adiciona try/catch/ die

--- conecta.php
+++ conecta.php
@@ -8,10 +8,23 @@ $banco = "vendas";
 
 /* Configurações para conexão ao banco de dados  */
 
-// Criando conexão com o banco usando a classe PDO 
+/* Bloco try/cathc 
+Passamos no try tudo que queremos que sej afeito (as ações de confi/conexão com BD) */
+try { 
+    // Criando conexão com o banco usando a classe PDO 
 // PDO (PHP Data Object): classe para manipulação de banco de dados
 $conexao = new PDO( 
     "mysql:host=$servidor;dbname=$banco;charset=utf8", 
-    $usuario, $senha
+    $usuario, $senha  
+); 
 
-);
+/* Confirgurar o PDO para lançar exceções/erros caso ocorram*/
+ 
+   
+     $conexao->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $erro) { 
+    /* E colocamos no catche o que fazer CASO alguma ação no try FALHE. 
+    Neste caso, é gerada uma exceção/erro e exibimos a mensagem sobre ela.  */
+    die( "Deu ruim:".$erro->getMessage() );
+}      
+
